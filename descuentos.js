3c012e67c7c341155f820549bb8d2d295593e2c7
Aaron Delgado <radees.24.16@gmail.com>
2022-03-30 14:06:58 -0500

    complete descuent

--- descuentos.js
+++ descuentos.js
@@ -10,4 +10,38 @@ function PrecioConDescuento(){
     document.getElementById("resultado").innerText = resultado;   
 }
 
+//DESCUENTO CUPON
+let estado = true;
+    function DesapareceAparece(){
+if(estado == true){
+    document.getElementById("desaparece").style.display = "block";
+    estado = false;
+}else{
+    document.getElementById("desaparece").style.display = "none";
+    estado = true;
+}
+}
+let valor = 0;
+function doce(){
+    valor = 12;
+    document.getElementById("desaparece").style.display = "none";
+    estado = true;
+}
+function veinte(){
+    valor = 20;
+    document.getElementById("desaparece").style.display = "none";
+    estado = true;
+}
+function veintiCinco(){
+    valor = 25;
+    document.getElementById("desaparece").style.display = "none";
+    estado = true;
+}
+function PrecioConDescuentoCupon(){
+    const inputPrecio = document.getElementById("precio1").value;
+    const EligeDescuento = CalculaDescuento(inputPrecio, valor);
+    document.getElementById("resultado1").innerText = EligeDescuento;
+    valor = 0;
+}
+
 
