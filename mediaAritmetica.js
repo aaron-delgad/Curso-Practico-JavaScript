1e46b2c0a51758d4d0e65165df6bf373330709f2
Aaron Delgado <radees.24.16@gmail.com>
2022-04-07 14:56:02 -0500

    update

--- mediaAritmetica.js
+++ mediaAritmetica.js
@@ -7,7 +7,7 @@ function mediaAritmetca(lista){
 }
 
 function CalculaMediaAritmetica(){
-    const inputLista1 = document.getElementById("media1").value;
+    const inputLista1 = document.getElementById("media1").Value;
     let array = inputLista1.split(','); 
     const sumaLista = mediaAritmetca(array);
     const NTotal = array.length;
