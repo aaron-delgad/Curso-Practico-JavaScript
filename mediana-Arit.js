2bfb6b5bef2d1d418fdd56a9c6bf0b7a075e64ab
Aaron Delgado <radees.24.16@gmail.com>
2022-04-07 14:51:17 -0500

    actual

--- mediana-Arit.js
+++ mediana-Arit.js
@@ -1,6 +1,6 @@
 
 function CalculaMediaAritmetica(){
-    const inputLista1 = document.getElementById("media1").value;
+    const inputLista1 = document.getElementById("media1").ariaValueMax;
     document.getElementById("").valu
     const re = inputMedi.va;
     alert(inputArray);
